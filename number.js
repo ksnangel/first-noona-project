07a5a29d145725a8922358bcf86e11d86715ad33
ksnangel <koreakimangel@gmail.com>
2022-01-19 00:59:18 +0900

    first commit

---
+++ number.js
@@ -0,0 +1,23 @@
+const age = 30 ; // 숫자형 Number
+const PI = 3.14 ;
+
+console.log(1 + 2); //더하기
+console.log(10 - 3); //빼기
+console.log(3 * 2); //*곱하기
+console.log(6 / 3); // /나누기
+console.log(6 % 4); // % 나머지
+
+const name = "Mike";
+const y = name/2;
+
+console.log(y)
+
+// NaN = Not a number
+
+//Boolean
+
+const a = true; //참
+const b = false; //거짓
+
+cosole. log(name == "Mike")
+cosole. log(PI > 40)
